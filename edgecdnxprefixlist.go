b1bc43fd381ec6c7e4261879051b244f70516f6f
fr6nco <tomas.boros92@gmail.com>
2025-05-20 12:37:54 +0200

    Implement metadata routing based on avl tree

--- edgecdnxprefixlist.go
+++ edgecdnxprefixlist.go
@@ -30,50 +30,8 @@ type EdgeCDNXPrefixList struct {
 type EdgeCDNXPrefixListResponseWriter struct {
 }
 
-// ServeDNS implements the plugin.Handler interface. This method gets called when example is used
-// in a Server.
 func (e EdgeCDNXPrefixList) ServeDNS(ctx context.Context, w dns.ResponseWriter, r *dns.Msg) (int, error) {
-	// This function could be simpler. I.e. just fmt.Println("example") here, but we want to show
-	// a slightly more complex example as to make this more interesting.
-	// Here we wrap the dns.ResponseWriter in a new ResponseWriter and call the next plugin, when the
-	// answer comes back, it will print "example".
-
-	// Debug log that we've have seen the query. This will only be shown when the debug plugin is loaded.
-
 	return plugin.NextOrFailure(e.Name(), e.Next, ctx, w, r)
-
-	// state := request.Request{W: w, Req: r}
-
-	// qname := state.Name()
-	// qtype := state.Type()
-
-	// log.Debug(fmt.Sprintf("edgecdnxprefixlist: %s %s", qname, qtype))
-
-	// res := new(dns.A)
-	// res.Hdr = dns.RR_Header{Name: "google.com.", Rrtype: dns.TypeA, Class: dns.ClassINET, Ttl: 180}
-	// res.A = net.IPv4(192, 168, 0, 100) // Example IP address
-
-	// m := new(dns.Msg)
-	// m.SetReply(r)
-	// m.Authoritative = true
-	// m.Answer = append(m.Answer, res)
-	// state.SizeAndDo(m)
-	// m = state.Scrub(m)
-
-	// log.Debug(fmt.Sprintf("edgecdnxprefixlist: %v", m))
-
-	// err := w.WriteMsg(m)
-	// if err != nil {
-	// 	log.Error(fmt.Sprintf("edgecdnxprefixlist: %v", err))
-	// 	return dns.RcodeServerFailure, err
-	// }
-	// return dns.RcodeSuccess, nil
-
-	// // Export metric with the server label set to the current server handling the request.
-	// // requestCount.WithLabelValues(metrics.WithServer(ctx)).Inc()
-
-	// // Call next plugin (if any).
-	// return plugin.NextOrFailure(e.Name(), e.Next, ctx, w, r)
 }
 
 func (g EdgeCDNXPrefixList) Metadata(ctx context.Context, state request.Request) context.Context {
@@ -88,40 +46,38 @@ func (g EdgeCDNXPrefixList) Metadata(ctx context.Context, state request.Request)
 			}
 		}
 	}
+	log.Debug(fmt.Sprintf("Setting up static routing meta for request incoming from %s\n", srcIP))
 
-	log.Debug(fmt.Sprintf("srcIP %s\n", srcIP))
-
-	if srcIP.To4() != nil {
-		dest := g.Routing.RoutingV4.Find(PrefixTreeEntry{
-			Prefix: net.IPNet{
-				IP:   srcIP,
-				Mask: net.CIDRMask(32, 32),
-			},
-		})
-
-		if dest != nil {
-			log.Debug(fmt.Sprintf("Found V4 prefix %s", dest))
-			metadata.SetValueFunc(ctx, g.Name()+"/location", func() string {
-				return dest.(PrefixTreeEntry).Location
+	metadata.SetValueFunc(ctx, g.Name()+"/location", func() string {
+		if srcIP.To4() != nil {
+			dest := g.Routing.RoutingV4.Find(PrefixTreeEntry{
+				Prefix: net.IPNet{
+					IP:   srcIP,
+					Mask: net.CIDRMask(32, 32),
+				},
 			})
-		}
-	}
 
-	if srcIP.To16() != nil {
-		dest := g.Routing.RoutingV6.Find(PrefixTreeEntry{
-			Prefix: net.IPNet{
-				IP:   srcIP,
-				Mask: net.CIDRMask(128, 128),
-			},
-		})
-
-		if dest != nil {
-			log.Debug(fmt.Sprintf("Found V6 prefix %s", dest))
-			metadata.SetValueFunc(ctx, g.Name()+"/location", func() string {
+			if dest != nil {
+				log.Debug(fmt.Sprintf("Found V4 prefix %s", dest))
 				return dest.(PrefixTreeEntry).Location
-			})
+			}
 		}
-	}
+
+		if srcIP.To16() != nil {
+			dest := g.Routing.RoutingV6.Find(PrefixTreeEntry{
+				Prefix: net.IPNet{
+					IP:   srcIP,
+					Mask: net.CIDRMask(128, 128),
+				},
+			})
+
+			if dest != nil {
+				log.Debug(fmt.Sprintf("Found V6 prefix %s", dest))
+				return dest.(PrefixTreeEntry).Location
+			}
+		}
+		return ""
+	})
 
 	return ctx
 }
